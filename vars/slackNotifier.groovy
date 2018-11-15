7cac2f9c65d814a77864b8e8c36eb825ba6b7672
caost <caost.uni@gmail.com>
2018-11-15 14:18:33 +0900

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,7 +1,9 @@
 #!/usr/bin/env groovy
 
 def call(String buildResult) {
-  if ( buildResult == "SUCCESS" ) {
+  if ( buildResult == "START" ) {
+    slackSend color: "warning", message: "Job: `${env.JOB_NAME}` with #${env.BUILD_NUMBER} is start build"
+  } else if ( buildResult == "SUCCESS" ) {
     slackSend color: "good", message: "Job: `${env.JOB_NAME}` with #${env.BUILD_NUMBER} was successful"
   }
   else if( buildResult == "FAILURE" ) { 
